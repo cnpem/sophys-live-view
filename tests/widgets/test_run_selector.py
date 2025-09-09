096877b6ff3d582dd6bb27767546980b6ee5affa
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2025-09-09 10:41:15 -0300

    tests: fix run selector multiple selection test
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- tests/widgets/test_run_selector.py
+++ tests/widgets/test_run_selector.py
@@ -38,11 +38,19 @@ def test_run_selector_select_multiple(selector, data_source_manager, qtbot):
     with qtbot.waitSignals([data_source_manager.new_data_stream] * 2, timeout=1000):
         data_source_manager.start()
 
-    with qtbot.waitSignal(selector.selected_streams_changed, timeout=1000) as blocker:
-        selector._run_list.setCurrentRow(
-            0, QItemSelectionModel.SelectionFlag.ClearAndSelect
+    selection_model = selector._run_list.selectionModel()
+
+    def select(row: int, flag: QItemSelectionModel.SelectionFlag):
+        selection_model.select(
+            selector._run_list.indexFromItem(selector._run_list.item(row)), flag
         )
-        selector._run_list.setCurrentRow(1, QItemSelectionModel.SelectionFlag.Select)
-    assert len(blocker.args[0]) == 2, blocker.args[0]
-    assert blocker.args[0][0][1] == "abc", blocker.args[0]
-    assert blocker.args[0][1][1] == "ghi", blocker.args[0]
+
+    with qtbot.waitSignal(selector.selected_streams_changed, timeout=1000):
+        select(0, QItemSelectionModel.SelectionFlag.SelectCurrent)
+    with qtbot.waitSignal(selector.selected_streams_changed, timeout=1000) as blocker:
+        select(1, QItemSelectionModel.SelectionFlag.Toggle)
+
+    args = blocker.args
+    assert len(args[0]) == 2, args[0]
+    assert args[0][0][1] == "abc", args[0]
+    assert args[0][1][1] == "ghi", args[0]
