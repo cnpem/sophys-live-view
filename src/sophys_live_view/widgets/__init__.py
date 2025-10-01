36cac71ed1dc420fbcdbfa6df097fed7acca87f4
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2025-10-01 13:52:11 -0300

    feat: add widget component imports in __init__ for ease of use
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

---
+++ src/sophys_live_view/widgets/__init__.py
@@ -0,0 +1,4 @@
+from .metadata_viewer import MetadataViewer  # noqa: F401
+from .plot_display import PlotDisplay  # noqa: F401
+from .run_selector import RunSelector  # noqa: F401
+from .signal_selector import SignalSelector  # noqa: F401
